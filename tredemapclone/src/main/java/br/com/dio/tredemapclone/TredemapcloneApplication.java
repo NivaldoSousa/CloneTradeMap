ea0f751aaf554dbb7e1d87b7f02a6adf7f37b344
NGA Sousa <nillsousa026@gmail.com>
2021-01-28 22:35:46 -0300

    Inicializaçao do projeto

---
+++ tredemapclone/src/main/java/br/com/dio/tredemapclone/TredemapcloneApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.tredemapclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TredemapcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TredemapcloneApplication.class, args);
+	}
+
+}
---
+++ tredemapclone/src/test/java/br/com/dio/tredemapclone/TredemapcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.tredemapclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TredemapcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
